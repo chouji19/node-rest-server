caeae4d09647ac4b60388db4ae82e37dc4f53756
Camilo <camilobu1.9@gmail.com>
2019-07-14 22:49:34 +1000

    Se agrega la parte de productos y categorias

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ process.env.URLDB = urlDB;
 //  Usos del token
 //
 // Vencimiento 
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.CADUCIDAD_TOKEN = '48h';
 //seed autenticacion
 process.env.SEED = process.env.SEED || 'estes-es-el-seed-desarollo';
 
